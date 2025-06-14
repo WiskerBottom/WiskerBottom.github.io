7adf1d4e91f2b005113e86ebe3440646e84a7dae
WiskerBottom <lucasthomaslessard@gmail.com>
2025-06-14 01:50:28 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-cat
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+app.run(debug=True, host="127.0.0.1")
